dddb1d1cc9a570762db60f07df4028c5e4bf2ef0
MATI UL REHMAN <iammatiz00@gmail.com>
2023-06-15 18:54:09 +0500

    pic uploading added;

--- lib/dashboardscreen.dart
+++ lib/dashboardscreen.dart
@@ -1,9 +1,14 @@
 import 'dart:developer';
 import 'dart:ffi';
+import 'dart:io';
+import 'dart:math';
 
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sp20_bse_042_terminal/loginscreen.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -17,6 +22,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController ageController = TextEditingController();
+  File? profilePic;
 
   void signOut() async {
     await FirebaseAuth.instance.signOut();
@@ -32,18 +38,28 @@ class _DashboardScreenState extends State<DashboardScreen> {
       nameController.clear();
       emailController.clear();
       ageController.clear();
-      if (name != '' && email != '' && age>0) {
+      if (name != '' && email != '' && age>0 && profilePic!=null) {
+        Random random = Random();
+        int randomNumber = random.nextInt(1000);
+        int randomNumber1 = random.nextInt(10000);
+        UploadTask uploadTask = FirebaseStorage.instance.ref().child('profiles').child('pic$randomNumber$randomNumber1').putFile(profilePic!);
+        TaskSnapshot taskSnapshot=await uploadTask;
+        String imageUrl=await taskSnapshot.ref.getDownloadURL();
         await FirebaseFirestore.instance.collection('users').add({
           "name": name,
           "email": email,
-          "age":age
+          "age":age,
+          "profilePic":imageUrl
         });
         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Record Added')));
       } else {
         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Invalid action')));
       }
+      setState(() {
+        profilePic=null;
+      });
     } catch (error) {
-      log(error.toString());
+      print(error.toString());
     }
   }
 
@@ -52,7 +68,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
       await FirebaseFirestore.instance.collection('users').doc(id).delete();
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('You have successfully deleted your data')));
     } catch (e) {
-      log(e.toString());
+      print(e.toString());
     }
   }
 
@@ -75,43 +91,53 @@ class _DashboardScreenState extends State<DashboardScreen> {
           padding: const EdgeInsets.all(15),
           child: Column(
             children: [
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    hintText: "Name",
-                  ),
+              CupertinoButton(
+                onPressed: ()async{
+                  XFile? selectedImage=await ImagePicker().pickImage(source: ImageSource.gallery);
+                  if(selectedImage!=null){
+                    File cFile=File(selectedImage.path);
+                    setState(() {
+                      profilePic=cFile;
+                    });
+                    print("Image selected");
+                  }else{
+                    print("No image selected");
+                  }
+                },
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  backgroundImage: (profilePic != null) ? FileImage(profilePic!) : null,
+                  radius: 50,
+                ),
+              ),
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  hintText: "Name",
                 ),
               ),
               const SizedBox(height: 10),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    hintText: "Email",
-                  ),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  hintText: "Email",
                 ),
               ),
-              const SizedBox(height: 10),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: ageController,
-                  decoration: const InputDecoration(
-                    hintText: "Age",
-                  ),
+              const SizedBox(height: 5),
+              TextField(
+                controller: ageController,
+                decoration: const InputDecoration(
+                  hintText: "Age",
                 ),
               ),
-              const SizedBox(height: 10),
+              const SizedBox(height: 5),
               TextButton(
                 onPressed: () {
                   addUsers();
                 },
                 child: const Text('Save Data'),
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 5),
               StreamBuilder(
                 stream: FirebaseFirestore.instance.collection('users').where("age",whereNotIn: [22,34,35]).orderBy("age",descending: true).snapshots(),
                 builder: (context, snapshot) {
@@ -122,9 +148,11 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         itemBuilder: (context, index) {
                           Map<String, dynamic> userMap = snapshot.data!.docs[index].data();
                           return ListTile(
-                            title: Text(userMap["email"]),
+                            title: Text(userMap["name"]),
                             subtitle: Text("Age ${userMap["age"]}"),
-                            leading: Text(userMap["name"]),
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(userMap["profilePic"]),
+                            ),
                             trailing: IconButton(
                               icon: const Icon(Icons.delete),
                               onPressed: () {
